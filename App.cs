518bcab162a71ad6a8d8cf568b71c903c3f81538
Mohammad Moghrabi <mohamad.isa.moghrabi@gmail.com>
2023-07-23 21:35:24 +0300

    started the project.
    Validator class to check the validation of the command and excute it.
    App class to interact with the user.

---
+++ App.cs
@@ -0,0 +1,37 @@
+﻿using SimpleInventoryManagementSystem;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+internal class App
+{
+   
+    
+    private static void Main(string[] args)
+    {
+        const String commandFormat = "Here are the commands you can use: \n"
+            + "add <product name> <price> <quantity>: to add new product \n" +
+            "edit <name>: to edit an existing product \n" +
+            "search <product name>: to search about a product \n" +
+            "view all: to view all product \n" +
+            "delete <procudt name>: to delete a product ";
+        String? command = String.Empty;
+        Console.WriteLine("Welcome to Simple Inventory Management System! \n \n" + commandFormat);
+        while (command != "exit") 
+        {
+            command = Console.ReadLine();
+            if (!Validator.validate(command))
+            {
+                Console.WriteLine("Please write the correct command format \n" + commandFormat);
+            }
+            else
+            {
+                Validator.ExcuteCommand(command);
+            }
+
+            
+        }
+    }
+
+   
+}
---
+++ Validator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem
+{
+    public static class Validator
+    {
+        public static Boolean validate(String command)
+        {
+            HashSet<String> commands = new HashSet<string>() { "add", "edit", "search", "view all", "delete", "exist" };
+            try{
+                int.Parse(command.Split(" ")[3]);
+                int.Parse(command.Split(" ")[2]);
+            }
+            catch(Exception e){
+                return false;
+            }
+            if (command == null)
+            {
+                return false;
+            }
+            else if (!commands.Contains(command.Split(" ")[0]))
+            {
+                return false;
+            }
+            else { return true; }
+           
+        }
+
+        public static void ExcuteCommand(string command)
+        {
+
+            Product product = new Product();
+            if (command.Split(" ")[0] == "add")
+            {
+                product.name = command.Split(" ")[1];
+                product.price = int.Parse(command.Split(" ")[2]);
+                product.quantity = int.Parse(command.Split(" ")[3]);
+               
+            }
+            Console.WriteLine(Inventory.Add(product));
+        }
+    }
+}
